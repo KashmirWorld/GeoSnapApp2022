e909be32d16f62eb3a0938deccc271460b3a679e
Abimalek Mekuriya <82734267+abimek@users.noreply.github.com>
2022-07-12 12:19:29 -0400

    Revert "- Removed 2 unused methods"

--- app/src/main/java/org/kashmirworldfoundation/WildlifeGeoSnap/profile/ProfileFragment.java
+++ app/src/main/java/org/kashmirworldfoundation/WildlifeGeoSnap/profile/ProfileFragment.java
@@ -38,6 +38,7 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.google.gson.Gson;
 
+import org.kashmirworldfoundation.WildlifeGeoSnap.GlideApp;
 import org.kashmirworldfoundation.WildlifeGeoSnap.auth.user.LoginActivity;
 import org.kashmirworldfoundation.WildlifeGeoSnap.firebase.FirebaseHandler;
 import org.kashmirworldfoundation.WildlifeGeoSnap.firebase.types.Member;
@@ -46,10 +47,10 @@ import org.kashmirworldfoundation.WildlifeGeoSnap.R;
 import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Type;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements View.OnClickListener{
     private static final int PICK_IMAGE = 100;
-    private TextView Pname,Pphone,Pemail,Padmin;
-    private View fragmentView;
+    private TextView Pname,Pphone,Padmin,Pemail;
+    private View fragmentView ;
     private ImageView Ppic;
     private Button logout;
     private FirebaseFirestore Fstore;
@@ -58,71 +59,32 @@ public class ProfileFragment extends Fragment {
     private ImageButton Mupload;
     private Uri imageurl;
     private String Uid;
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Fstorage = FirebaseStorage.getInstance();
+        Fstorage= FirebaseStorage.getInstance();
         fragmentView = inflater.inflate(R.layout.fragment_profile, container, false);
-        Fstore = FirebaseFirestore.getInstance();
-        Fauth = FirebaseAuth.getInstance();
-
+        Fstore=FirebaseFirestore.getInstance();
+        Fauth=FirebaseAuth.getInstance();
+        Pname=(TextView)fragmentView.findViewById(R.id.NameP);
+        Pphone=(TextView)fragmentView.findViewById(R.id.PhoneP);
+        Padmin =(TextView)fragmentView.findViewById(R.id.AdminP);
+        Pemail =(TextView)fragmentView.findViewById(R.id.EmailP);
+        Ppic = (ImageView) fragmentView.findViewById(R.id.ProfilePic);
+        Mupload= (ImageButton) fragmentView.findViewById(R.id.Pupload);
+        Uid=Fauth.getCurrentUser().getUid();
         Member member = Member.getInstance();
-        update_Pname(member);
-        update_Pphone(member);
-        update_Pemail(member);
-        update_Padmin(member);
-
-        update_ProfilePic(member);
-        update_Pupload();
-        logoutUser();
-        return fragmentView;
-    }
-
-    public void update_Pname(Member member) {
-        Pname = (TextView)fragmentView.findViewById(R.id.NameP);
         Pname.setText("Name: " + member.getFullname());
-    }
-
-    public void update_Pphone(Member member) {
-        Pphone = (TextView)fragmentView.findViewById(R.id.PhoneP);
         Pphone.setText("Phone: " + member.getPhone());
-    }
-
-    public void update_Pemail(Member member) {
-        Pemail = (TextView)fragmentView.findViewById(R.id.EmailP);
         Pemail.setText("Email: " + member.getEmail());
-    }
-
-    public void update_Padmin(Member member) {
-        Padmin = (TextView)fragmentView.findViewById(R.id.AdminP);
-        if (member.isAdmin()) {
+        if (member.isAdmin()){
             Padmin.setText("Admin: True");
         }
-        else {
+        else{
             Padmin.setVisibility(View.INVISIBLE);
         }
-    }
-
-    public void update_ProfilePic(Member member) {
-        Ppic = (ImageView)fragmentView.findViewById(R.id.ProfilePic);
         FirebaseHandler.loadImageIntoView(member.getProfile(), Ppic, ProfileFragment.this);
-    }
 
-    public void update_Pupload() {
-        Mupload = (ImageButton)fragmentView.findViewById(R.id.Pupload);
-        Mupload.setOnClickListener(new View.OnClickListener() {
-            @Override // Opens gallery
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
-            }
-        });
-    }
-
-    public void logoutUser() {
         logout = (Button) fragmentView.findViewById(R.id.logoutP);
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -134,12 +96,40 @@ public class ProfileFragment extends Fragment {
                 startActivity(intent);
             }
         });
+        Mupload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openGallery();
+            }
+        });
+        return fragmentView;
+    }
+    private void openGallery(){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE);
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == Activity.RESULT_OK && requestCode == PICK_IMAGE) {
-            imageurl = data.getData();
+    public void onClick(View v) {
+        Button logout = (Button) fragmentView.findViewById(R.id.logoutP);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth fauth= FirebaseAuth.getInstance();
+                fauth.signOut();
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if (resultCode== Activity.RESULT_OK && requestCode ==PICK_IMAGE){
+            imageurl =data.getData();
             Ppic.setImageURI(imageurl);
             Ppic.setDrawingCacheEnabled(true);
             Ppic.buildDrawingCache();
@@ -148,26 +138,34 @@ public class ProfileFragment extends Fragment {
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
             byte[] datan = baos.toByteArray();
-            uploadTask_Helper(datan);
+            StorageReference profile= Fstorage.getReference("profile/" +Uid);
+            UploadTask uploadTask = profile.putBytes(datan);
+            uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    if (task.isSuccessful()){
+                        Fstore.collection("Member").document(Uid).update("profile","profile/"+Uid);
+                    }
+                    else{
+                        Toast.makeText(getContext(),"Error adding profile please try again with a stable connection",Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+
+
         }
-        super.onActivityResult(requestCode,resultCode,data);
+    }
+    public static void createToast(Context context, String message, int time) {
+        Toast toast = Toast.makeText(context, "" + message, time);
+        View toastView = toast.getView();
+        toastView.setBackgroundColor(Color.parseColor("#008577"));
+        TextView tv = toast.getView().findViewById(android.R.id.message);
+        tv.setPadding(140, 75, 140, 75);
+        tv.setTextColor(0xFFFFFFFF);
+        toast.show();
     }
 
-    public void uploadTask_Helper(byte[] datan) {
-        Uid = Fauth.getCurrentUser().getUid();
-        StorageReference profile= Fstorage.getReference("profile/" +Uid);
-        UploadTask uploadTask = profile.putBytes(datan);
-        uploadTask.addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
-                if (task.isSuccessful()) {
-                    Fstore.collection("Member").document(Uid).update("profile","profile/"+Uid);
-                }
-                else {
-                    Toast.makeText(getContext(),"Error adding profile please try again with a stable connection",Toast.LENGTH_LONG).show();
-                }
-            }
-        });
+    public static ProfileFragment newInstance() {
+        return new ProfileFragment();
     }
 }
-
