8ce23486336c6d42107f66c965c9de7660442799
LightningBoltz21 <84284132+LightningBoltz21@users.noreply.github.com>
2022-08-11 17:08:24 -0400

    progress on camera station activity layout

---
+++ app/src/main/java/org/kashmirworldfoundation/WildlifeGeoSnap/CameraStationMenuActivity.java
@@ -0,0 +1,14 @@
+package org.kashmirworldfoundation.WildlifeGeoSnap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraStationMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_station_menu);
+    }
+}
